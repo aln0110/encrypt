089a9df70195cac0f03eadbf33d4dd58a1db3b37
Allan Robinson Parkinson <allanrp0110@gmail.com>
2024-12-12 20:53:05 -0600

    updates now it generates a same hash for the same values

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ from toHash import Encrypt
 
 
 def main():
-    rand = apiRandResponse()
-    salt = Encrypt.getSalt()
-    nonce = Encrypt.getNonce()
+    #rand = apiRandResponse()
+    #salt = Encrypt.getSalt()
+    #nonce = Encrypt.getNonce()
+    #StrngHelper = Encrypt.randomString()
     StringToHash = "pichaMamaUsted"
     
-    #print(f'Randon: {rand} Salt: {salt} Nonce: {nonce}')
+
+    rand= 311025526
+    salt= 122670693681576847774163115726612019392
+    nonce= 199487788391418891040571943207132884477
+    StringHelper= "Qc0Hn4UEWUr0AB@rDLdB"
     
-    result = Encrypt.encrypt(StringToHash, salt, nonce, rand)
+    result = Encrypt.encrypt(StringToHash, salt, nonce, rand, StringHelper)
     
-    print (f'With this rand: {rand} with this salt: {salt} with this nonce: {nonce} the result is: {result}')
+    
+    print (f'With this rand: {rand} with this salt: {salt} with this nonce: {nonce} with this String helper: {StringHelper} the result is: {result}  and the size is: {len(result)}')
     
    #print(Patito.whoAmI())
    
--- toHash.py
+++ toHash.py
@@ -1,59 +1,80 @@
-from argon2 import PasswordHasher
+from argon2.low_level import hash_secret, Type
 import os
+import random
+
+
+
 class Encrypt:
-    def __init__(self, password, salt, nonce, rand,  ):
+    def __init__(
+        self,
+        password,
+        salt,
+        nonce,
+        rand,
+    ):
         self.password = password
-        self.salt =salt
+        self.salt = salt
         self.nonce = nonce
         self.rand = rand
-    
-    
+
     def getSalt():
-       rand= os.urandom(16)
-       rand2 = os.urandom(2)
-       sum = int.from_bytes(rand2, byteorder='big')
-       random = int.from_bytes(rand, byteorder='big')
-       
-       return random  + sum
-     
+        rand = os.urandom(16)
+        rand2 = os.urandom(2)
+        sum = int.from_bytes(rand2, byteorder="big")
+        random = int.from_bytes(rand, byteorder="big")
+
+        return random + sum
+
     def getNonce():
-       rand = os.urandom(16)
-       random = int.from_bytes(rand, byteorder='big') 
-       rand2 = os.urandom(2)
-       sum = int.from_bytes(rand2, byteorder='big')
-       return  random  + sum
+        rand = os.urandom(16)
+        random = int.from_bytes(rand, byteorder="big")
+        rand2 = os.urandom(2)
+        sum = int.from_bytes(rand2, byteorder="big")
+        return random + sum
     
-    def encrypt(Pass,salt, nonce, rand ):
-        ph = PasswordHasher()
+    def randomString():
+       pool ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+"
+       result = ""
+       for i in range(0,20):
+           rand = random.randint(1, len(pool))
+           result += pool[rand-1]             
+           pass
+       return result
+
+    def encrypt(Pass, salt, nonce, rand, StringHelper):
+        
         wordString = Pass + str(salt) + str(nonce)
 
-        
-        #hashing the wordString
+        # hashing the wordString
         hValue = 2166136261 + int(rand)
-        
+
         for i in range(len(wordString)):
             hValue = (hValue << 7) + hValue + ord(wordString[i])
 
             if hValue % 2 == 0:
-              hValue = hValue + 100
+                hValue = hValue + 100
 
             else:
-               hValue = hValue + 5
+                hValue = hValue + 5
 
             if hValue % 3 == 0:
-               hValue = hValue ^ ord(wordString[i])
-               hValue = hValue + 5
+                hValue = hValue ^ ord(wordString[i])
+                hValue = hValue + 5
 
             if hValue % 5 == 0:
-               hValue = hValue * ord(wordString[i])
-                
-                
-        hashed =  hex(hValue & 0xFFFFFFFFFFFFFFFF)      
-        #end hashing the wordString
-        
-        #hasing the wordString using argon2 but adding the random number to the string
-        hashHashed = ph.hash(hashed)  
-       #end hashing the wordString using argon2 but adding the random number to the string  
+                hValue = hValue * ord(wordString[i])
 
-         
-        return hashHashed
+        hashed = hex(hValue & 0xFFFFFFFFFFFFFFFF)
+        # end hashing the wordString
+        
+        
+
+        # hasing the wordString using argon2 but adding the random number to the string
+        StringHelper= hashed.encode('utf-8')
+        hashHashed = hash_secret(hashed.encode('utf-8'), StringHelper, time_cost=3, memory_cost=65536, parallelism=4, hash_len=32, type=Type.ID)
+        
+
+        # end hashing the wordString using argon2 but adding the random number to the string
+
+        #return hashed
+        return  hex(int.from_bytes(hashHashed, byteorder="big") & 0xFFFFFFFFFFFFFFFFFF)
